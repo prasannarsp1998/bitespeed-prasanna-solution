d3ce6aa158d751292df591dfa8032d3e1571ff80
prasanna-kilofarms <prasanna@kilofarms.com>
2024-03-28 13:45:21 +0530

    configuring new endpoint identify. CORS support implemented authentication for API

---
+++ config.py
@@ -0,0 +1,3 @@
+config = {
+    "x-api-key":['r3PJdg$j8>1d0n!d:B#Oh.2KAsiMO7/!!~ZH^Wn6lp+qW9Trkc'],
+}
--- main.py
+++ main.py
@@ -9,6 +9,38 @@ application = flask_app
 import mysql.connector
 import requests
 
+def validate_api(token):
+    if token not in config.config['x-api-key']:
+        return 401
+
+@flask_app.route("/api/v1/identify", methods=["POST", "OPTIONS"])
+def api_allapplications_insert():
+    if request.method == "OPTIONS": # CORS preflight
+        return _build_cors_preflight_response()
+    elif request.method == "POST":
+        # Check if API key present
+        if 'Authorization' not in request.headers:
+            return _corsify_actual_response(jsonify({'response': 'failure', 'message': 'API Key Required'})), 401
+
+        # Check if API key is valid
+        if validate_api(request.headers['Authorization'].split(' ')[1]) == 401:
+            return _corsify_actual_response(jsonify({'response': 'failure', 'message': 'In-valid Token'})), 401
+
+
+        return _corsify_actual_response(jsonify({'response': 'success'})), 201
+
+
+def _build_cors_preflight_response():
+    response = make_response()
+    response.headers.add("Access-Control-Allow-Origin", "*")
+    response.headers.add('Access-Control-Allow-Headers', "*")
+    response.headers.add('Access-Control-Allow-Methods', "*")
+    return response
+
+def _corsify_actual_response(response):
+    response.headers.add("Access-Control-Allow-Origin", "*")
+    return response
+
 
 if __name__ == '__main__':
     flask_app.run(host= '0.0.0.0',port=678, debug=True)
